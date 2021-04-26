0c91b1517231c7743085ba8442a8f7402ef785cd
Ganesh <ganuofficial2795@gmail.com>
2021-04-26 13:06:33 +0530

    Face detected

--- DetectFace.py
+++ DetectFace.py
@@ -6,4 +6,31 @@ from random import randrange
 face_detect_data = cv.CascadeClassifier('DataSet.xml')
 
 
-print("Code Completed 🔥")
+def Detect_faces():
+    """This function help us to detect faces on the given Image by converting the color image into grayScale"""
+
+    try:
+        # actual image
+        img = cv.imread('Images\Elon.jpg')
+        cv.imshow("Elon-Face",img)
+
+        # grayScale images
+        gray = cv.cvtColor(img,cv.COLOR_RGB2GRAY)
+        cv.imshow("Gray-Face-Elon",gray)
+
+        # detecting the faces
+
+        face_coordinates = face_detect_data.detectMultiScale(gray)
+        #print(len(face_coordinates))
+        # print(face_coordinates)
+
+    except Exception as e:
+        print("Something went wrong --> ",e)
+
+
+if __name__ == "__main__":
+    Detect_faces()
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    print("Code Completed 🔥")
